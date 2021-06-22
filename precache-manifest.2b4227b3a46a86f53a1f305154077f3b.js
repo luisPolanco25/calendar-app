9ce3df6d63a5119336550e3b7f024a38ed33ad94
LuisP <luisjavierpolancomorel@gmail.com>
2021-06-22 00:20:39 -0400

    Updates

---
+++ precache-manifest.2b4227b3a46a86f53a1f305154077f3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3206a005dda17379bbb502c15ab88269",
+    "url": "/calendar-app/index.html"
+  },
+  {
+    "revision": "6cfa408800bcdcab9cac",
+    "url": "/calendar-app/static/css/2.74ad4f72.chunk.css"
+  },
+  {
+    "revision": "1952757e041b641eb6f3",
+    "url": "/calendar-app/static/css/main.a36c8a9a.chunk.css"
+  },
+  {
+    "revision": "6cfa408800bcdcab9cac",
+    "url": "/calendar-app/static/js/2.943c7faf.chunk.js"
+  },
+  {
+    "revision": "897eb22b719834bb5b78dee71af66bba",
+    "url": "/calendar-app/static/js/2.943c7faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1952757e041b641eb6f3",
+    "url": "/calendar-app/static/js/main.6dda455c.chunk.js"
+  },
+  {
+    "revision": "374679d84b9c7979fb62",
+    "url": "/calendar-app/static/js/runtime-main.d170d573.js"
+  }
+]);
